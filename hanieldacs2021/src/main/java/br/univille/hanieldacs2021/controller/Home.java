9cf95311736ce0658884eabc0014c6b755fbe5e7
Haniel Lourenço Lohn <hanielohn2016@gmail.com>
2021-06-14 23:08:10 +0000

    ADD: templates

--- hanieldacs2021/src/main/java/br/univille/hanieldacs2021/controller/Home.java
+++ hanieldacs2021/src/main/java/br/univille/hanieldacs2021/controller/Home.java
@@ -1,21 +1,36 @@
 package br.univille.hanieldacs2021.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class Home {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        return "Eu não acredito";
+    @GetMapping
+    public ModelAndView index() {
+        Date dataAgora = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+
+        HashMap<String, String> dados = new HashMap<>();
+        dados.put("tempo", sdf.format(dataAgora));
+        dados.put("nomeapp", "App java lindo");
+
+        // return new ModelAndView("home/index", "tempo", sdf.format(dataAgora));
+        return new ModelAndView("home/index", dados);
     }
 
-    @RequestMapping("/outro")
-    @ResponseBody
-    public String outraPagina() {
-        return "outra página";
-    }
+    // @RequestMapping("/outro")
+    // @ResponseBody
+    // public String outraPagina() {
+    // return "outra página";
+    // }
 }
